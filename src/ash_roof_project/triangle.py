327a921e0a60ece2b46ceef9a4e4488befb268c0
DanRog-Chem <ed19dr@leeds.ac.uk>
2025-07-03 11:16:41 +0000

    Create pseudocode

---
+++ src/ash_roof_project/triangle.py
@@ -0,0 +1,10 @@
+# Module to calc hypot of triangle
+
+# Hypot calc function
+# takes lengths a and b (not hypot) of a triangle and returns c (hypot)
+
+# square func
+# takes length a returns a^2  
+
+# adding func
+# takes a and b returns a + b
